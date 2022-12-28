5cb6c0aa73ea35e4aab785a6e988e580c55a5385
Yor-Jihons <ryo.malon814@gmail.com>
2022-12-28 20:16:34 +0900

    Added the test for pairs.ts.

---
+++ __tests__/pair.test.ts
@@ -0,0 +1,13 @@
+import * as Pairs from '../src/pairs';
+
+describe('Test for pairs', () => {
+    test('sample1', () => {
+        let pair = new Pairs.Pair( "key1", "value1" );
+        expect( pair.getKey() ).toBe( "key1" );
+    });
+
+    test('sample2', () => {
+        let pair = new Pairs.Pair( "key1", "value1" );
+        expect( pair.getValue() ).toBe( "value1" );
+    });
+});
